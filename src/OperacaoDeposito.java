4e80b08a5f72e31418dded97269eeb7167c1f523
Júlia Martins Souza <juliamss07@gmail.com>
2024-06-09 19:59:49 -0300

    Update OperacaoDeposito.java

--- src/OperacaoDeposito.java
+++ src/OperacaoDeposito.java
@@ -2,4 +2,9 @@ public class OperacaoDeposito extends Operacao{
     public OperacaoDeposito(double valor) {
         super('d', valor);
     }
+
+     public String toString() {
+        String str = this.getData() + "\t" + this.getTipo() + "\t" + this.getValor();
+        return str;
+    }
 }
