c5e7fe1599d90e0f0f57a2fb7fffff21967a9f8e
Júlia Martins Souza <juliamss07@gmail.com>
2024-06-09 20:28:46 -0300

    finish main

--- src/Main.java
+++ src/Main.java
@@ -8,19 +8,19 @@ public class Main {
         PessoaFisica pF2 = new PessoaFisica("Mariana", "rua das princesas", data, "107.010.076-56", 16, 'F');
         PessoaJuridica pJ = new PessoaJuridica("CHURRASIC PARK", "rua dos dinossauros", data, "24 405 890/0001-86", "Alimenticio", 5);
 
-        Conta contaPF1 = new Conta(pF1);
-        Conta contaPF2 = new Conta(pF2);
-        Conta contaPJ = new Conta(pJ);
+        Conta contaPF1 = new Conta(pF1, 100000, 80000, 0);
+        Conta contaPF2 = new Conta(pF2, 50000, 10000, 1);
+        Conta contaPJ = new Conta(pJ, 500000, 100000, 2);
 
-        System.out.println("BANCO JMSZ _ IMPRIMINDO IMPORMACOES DOS CLIENTES");
-        System.out.println("\nConta 1");
-        pF1.imprimir();
-        System.out.println("\nConta 2");
-        pF2.imprimir();
-        System.out.println("\nConta 3");
-        pJ.imprimir();
+        System.out.println("<< BANCO JMSZ >>\n<< IMPRIMINDO INFORMACOES DOS CLIENTES >>");
+        System.out.println("\n<< Conta " + pF1.getNome() + " >>");
+            System.out.println(contaPF1.toString());
+        System.out.println("\n<< Conta " + pF2.getNome() + " >>");
+            System.out.println(contaPF2.toString());
+        System.out.println("\n<< Conta " + pJ.getNome() + " >>");
+            System.out.println(contaPJ.toString());
 
-        System.out.println("\n<<<REALIZANDO OPERACOES>>>");
+        System.out.println("\n<< REALIZANDO OPERACOES >>");
         contaPF1.depositar(5000);
         contaPF2.depositar(2000);
         contaPJ.depositar(10000);
@@ -29,17 +29,21 @@ public class Main {
         contaPF2.sacar(1500);
         contaPJ.sacar(1640);
 
-        System.out.println("\n<<IMPRIMINDO EXTRATO...>>");
-        System.out.println("\nConta " +pF1.getNome());
+        System.out.println("\n<< IMPRIMINDO EXTRATO... >>");
+        System.out.println("\nConta: " +pF1.getNome());
         contaPF1.extrato();
-        System.out.println("\nConta " +pF2.getNome());
+        System.out.println("\nConta: " +pF2.getNome());
         contaPF2.extrato();
-        System.out.println("\nConta " +pJ.getNome());
+        System.out.println("\nConta: " +pJ.getNome());
         contaPJ.extrato();
 
         System.out.println("\nImprimindo a media...");
         int media = Operacao.totalOperacoes/Conta.totalContas;
 
         System.out.println("Media: " +media);
+        
+        //verificacao pelo metodo equals
+        System.out.println("\nConta " + pF1.getNome() + " equals (igual) Conta " + pF2.getNome() + "? ");
+        System.out.println(pF1.equals(pF2));
     }
 }
