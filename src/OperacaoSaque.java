7810cb3f80dade2288ccdb51793b1793855f277c
Júlia Martins Souza <juliamss07@gmail.com>
2024-06-09 20:00:06 -0300

    Update OperacaoSaque.java

--- src/OperacaoSaque.java
+++ src/OperacaoSaque.java
@@ -2,4 +2,9 @@ public class OperacaoSaque extends Operacao {
     public OperacaoSaque(double valor) {
         super('s', valor);
     }
+
+     public String toString() {
+        String str = this.getData() + "\t" + this.getTipo() + "\t" + this.getValor();
+        return str;
+    }
 }
